1e8f222808802e4686c75671f8829ac0bef74c5c
Devanshu Modi <modidevanshu752@gmail.com>
2026-02-26 14:37:22 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,21 @@
+# Task1
+
+num1= int(input("Enter the first number: "))
+num2= int(input("Enter the second number: "))
+addtion = num1 + num2
+print("The sum is: ", addtion)
+subtraction = num1 - num2
+print("The difference is: ", subtraction)
+multiplication = num1 * num2
+print("The product is: ", multiplication)
+division = num1 / num2
+print("The quotient is: ", division)
+
+#Task2
+
+name1=input("Enter your first name: ")
+name2=input("Enter your last name: ")
+print("hello " + name1 + name2 + "! welcome to the python program" )
+
+
+
