eafc770643f9b4d8fec4c522b021ce44e38a6eaa
Devanshu Modi <modidevanshu752@gmail.com>
2026-02-27 10:55:22 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,8 @@
+
+
+p=float(input("enter initial principal balance: "))
+r=float(input("enter initial interest rate: "))
+t=float(input("enter time (in years): "))
+
+interest=(p*r*t)/100
+print("The interest is: ", interest)
